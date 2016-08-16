2e59d6da22a03b27276fd179e44cff7ba81aff07
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-08-16 12:46:24 +1200

    Including new registeries for test modules
    - Also reformatted the setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 version = '0.0'
 
-classifiers = """\
+classifiers = """
 Development Status :: 4 - Beta
 Environment :: Console
 Framework :: Setuptools Plugin
@@ -15,47 +15,50 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
-""".splitlines()
+""".strip().splitlines()
 
-setup(name='calmjs',
-      version=version,
-      description="Toolchain for deploying JavaScript with Python modules.",
-      long_description=open('README.rst').read(),
-      classifiers=classifiers,
-      keywords='',
-      author='Tommy Yu',
-      author_email='tommy.yu@auckland.ac.nz',
-      url='https://github.com/calmjs/',
-      license='GPL',
-      packages=find_packages('src', exclude=['ez_setup']),
-      package_dir={'': 'src'},
-      namespace_packages=['calmjs'],
-      zip_safe=True,
-      install_requires=[
-          'setuptools>=11.3',
-      ],
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
-      entry_points="""
-      # -*- Entry points: -*-
-      [distutils.commands]
-      npm = calmjs.npm:npm
-
-      [distutils.setup_keywords]
-      package_json = calmjs.dist:validate_json_field
-      extras_calmjs = calmjs.dist:validate_json_field
-
-      [egg_info.writers]
-      package.json = calmjs.npm:write_package_json
-      extras_calmjs.json = calmjs.dist:write_extras_calmjs
-
-      [calmjs.extras_keys]
-      node_modules = enabled
-
-      [calmjs.registry]
-      calmjs.extras_keys = calmjs.module:ExtrasJsonKeysRegistry
-      calmjs.registry = calmjs.registry:Registry
-      calmjs.module = calmjs.module:ModuleRegistry
-      calmjs.module.pythonic = calmjs.module:PythonicModuleRegistry
-      """,
-      test_suite="calmjs.tests.make_suite",
-      )
+setup(
+    name='calmjs',
+    version=version,
+    description="Toolchain for deploying JavaScript with Python modules.",
+    long_description=open('README.rst').read(),
+    classifiers=classifiers,
+    keywords='',
+    author='Tommy Yu',
+    author_email='tommy.yu@auckland.ac.nz',
+    url='https://github.com/calmjs/',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['calmjs'],
+    zip_safe=True,
+    install_requires=[
+        'setuptools>=11.3',
+    ],
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
+    entry_points={
+        'distutils.commands': [
+            'npm = calmjs.npm:npm',
+        ],
+        'distutils.setup_keywords': [
+            'package_json = calmjs.dist:validate_json_field',
+            'extras_calmjs = calmjs.dist:validate_json_field',
+        ],
+        'egg_info.writers': [
+            'package.json = calmjs.npm:write_package_json',
+            'extras_calmjs.json = calmjs.dist:write_extras_calmjs',
+        ],
+        'calmjs.extras_keys': [
+            'node_modules = enabled',
+        ],
+        'calmjs.registry': [
+            'calmjs.extras_keys = calmjs.module:ExtrasJsonKeysRegistry',
+            'calmjs.registry = calmjs.registry:Registry',
+            'calmjs.module = calmjs.module:ModuleRegistry',
+            'calmjs.module.pythonic = calmjs.module:PythonicModuleRegistry',
+            'calmjs.tests = calmjs.module:ModuleRegistry',
+            'calmjs.tests.pythonic = calmjs.module:PythonicModuleRegistry',
+        ],
+    },
+    test_suite="calmjs.tests.make_suite",
+)
