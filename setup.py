0a88e25ea2160e703f4e033860588eb6fd8a1882
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-08-29 17:17:31 +1200

    Support the wheel format
    - Following the standard distribution method (PEP-427)
    - Since code here are tested to work without 2to3, and no C extensions,
      universal eggs can be used.

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'setuptools>=11.3',
     ],
+    include_package_data=True,
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*',
     entry_points={
         'console_scripts': [
