calmjs/calmjs

commit 68e49cf1a57b53fe578c541b7f4ecc09f063e2d4

Correctly report unrecognized arguments
- Properly unwind all subparsers involved in the processing of the
  input arguments so that the correct set of unrecognized arguments be
  generated, rather than just doing the top layer which does result in
  the completely wrong program name being reported.
- Deprecated Runtime.error as that's now redundant.
- Defined a number of specific error handling functions on the Runtime
  and ArgumentParser classes for reporting of unrecognized arguments.
- Move ArgumentParserDetails to be a generic namedtuple on the runtime
  module (eliminates the albeit minor cost of different classes of the
  same signature).
- Updated README section on the unrecognized arguments issue.

src/calmjs/runtime.py
@@ -66,6 +66,9 @@ levels = {
 valid_command_name = re.compile('^[0-9a-zA-Z]*$')
 _global_runtime_attrs = {}
 
+ArgumentParserDetails = namedtuple('ArgumentParserDetails', [
+    'subparsers', 'runtimes', 'entry_points'])
+
 
 def _global_runtime_attrs_get(key):
     result = _global_runtime_attrs.get(key)
@@ -290,30 +293,16 @@ class BaseRuntime(BootstrapRuntime):
             warnings.simplefilter("always")
             parsed, extras = self.argparser.parse_known_args(args)
 
-        kwargs = vars(parsed)
-        target = kwargs.get(self.action_key)
-
-        if extras:
-            # first step, figure out where exactly the problem is, by
-            # taking everything before the target (if any) and see where
-            # the failure happened
-            before = args[:args.index(target)] if target in args else args
-            bootfail = bootstrap(before)
-            # msg has no gettext applied as in argparser module version
-            msg = 'unrecognized arguments: %s' % ' '.join(bootfail or extras)
-            if bootfail:
-                # failed on the root parser, deal with this here now.
-                self.argparser.error(msg)
-            else:
-                # let the implementation specific handling deal with it
-                self.error(self.argparser, target, msg)
-
         with pretty_logging(
                 logger=self.logger, level=self.log_level, stream=sys.stderr):
             # now that we have a logging scope, generate the logs.
             for record in records:
                 logger.warning(record.message)
+
             try:
+                if extras:
+                    self.unrecognized_arguments_error(args, parsed, extras)
+                kwargs = vars(parsed)
                 return self.run(argparser=self.argparser, **kwargs)
             except KeyboardInterrupt:
                 logger.critical('termination requested; aborted.')
@@ -359,8 +348,8 @@ class Runtime(BaseRuntime):
 
         self.entry_point_group = entry_point_group
         self.argparser_details = {}
-        self.ArgumentParserDetails = namedtuple('ArgumentParserDetails', [
-            'subparsers', 'runtimes', 'entry_points'])
+        # BBB compatibility
+        self.ArgumentParserDetails = ArgumentParserDetails
         super(Runtime, self).__init__(*a, **kw)
 
     def log_debug_error(self, *a, **kw):
@@ -425,8 +414,8 @@ class Runtime(BaseRuntime):
         def prepare_argparser():
             if argparser in self.argparser_details:
                 return False
-            result = self.argparser_details[
-                argparser] = self.ArgumentParserDetails({}, {}, {})
+            result = self.argparser_details[argparser] = ArgumentParserDetails(
+                {}, {}, {})
             return result
 
         def to_module_attr(ep):
@@ -477,7 +466,7 @@ class Runtime(BaseRuntime):
                     # to ensure that our thing would have been called.
                     cls = type(runtime)
                     logger.critical(
-                        "Runtime subclass at entry_point '%s' has overide "
+                        "Runtime subclass at entry_point '%s' has override "
                         "'entry_point_load_validated' without filtering out "
                         "its parent classes; this can be addressed by calling "
                         "super(%s.%s, self).entry_point_load_validated("
@@ -609,16 +598,54 @@ class Runtime(BaseRuntime):
         details = self.argparser_details.get(argparser)
         if details:
             return details
-        logger.error('provided argparser not registered to this runtime.')
+        logger.error(
+            'provided argparser (prog=%r) not associated with this '
+            'runtime (%r)', argparser.prog, self
+        )
+
+    def unrecognized_arguments_error(self, args, parsed, extras):
+        """
+        This exists because argparser is dumb and naive and doesn't
+        fail unrecognized arguments early.
+        """
+
+        # loop variants
+        kwargs = vars(parsed)
+        failed = list(extras)
+        # initial values
+        runtime, subparser, idx = (self, self.argparser, 0)
+        # recursion not actually needed when it can be flattened.
+        while isinstance(runtime, Runtime):
+            cmd = kwargs.pop(runtime.action_key)
+            action_idx = args.index(cmd)
+            subargs = args[idx:action_idx]
+            subparsed, subextras = subparser.parse_known_args(subargs)
+            if subextras:
+                subparser.unrecognized_arguments_error(subextras)
+                # since the failed arguments are in order
+                failed = failed[len(subextras):]
+
+            # advance the values
+            # note that any internal consistency will almost certainly
+            # result in KeyError being raised.
+            details = runtime.get_argparser_details(subparser)
+            runtime = details.runtimes[cmd]
+            subparser = details.subparsers[cmd]
+            idx = action_idx + 1
+
+        if failed:
+            subparser.unrecognized_arguments_error(failed)
+        sys.exit(2)
 
     def error(self, argparser, target, message):
         """
-        This is needed due to how the argparser may fail at deriving the
-        correct subcommand to include.  Although this BaseRuntime does
-        not implement this functionality, this method is reserved for
-        subclasses to handle that.
+        This was used as part of the original non-recursive lookup for
+        the target parser.
         """
 
+        warnings.warn(
+            'Runtime.error is deprecated and will be removed by calmjs-4.0.0',
+            DeprecationWarning)
         details = self.get_argparser_details(argparser)
         argparser = details.subparsers[target] if details else self.argparser
         argparser.error(message)

src/calmjs/tests/test_runtime.py
@@ -150,6 +150,18 @@ class BaseRuntimeTestCase(unittest.TestCase):
         result = rt.run(rt.argparser, runtime='dummy')
         self.assertIs(result, NotImplemented)
 
+    def test_runtime_error_deprecation(self):
+        stub_stdouts(self)
+        working_set = mocks.WorkingSet({'calmjs.runtime': [
+            'deprecated = calmjs.tests.test_runtime:deprecated',
+        ]})
+        rt = runtime.Runtime(working_set=working_set, prog='dummy')
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter('always')
+            with self.assertRaises(SystemExit):
+                rt.error(rt.argparser, 'deprecated', 'simulated')
+        self.assertIn('Runtime.error is deprecated', str(w[-1].message))
+
     def test_runtime_run_abort(self):
         class CustomRuntime(runtime.BaseRuntime):
             def run(self, export_target=None, **kwargs):
@@ -1127,7 +1139,7 @@ class PackageManagerRuntimeTestCase(unittest.TestCase):
         working_set = mocks.WorkingSet({'calmjs.runtime': [
             'npm = calmjs.npm:npm.runtime',
         ]})
-        rt = runtime.Runtime(working_set=working_set)
+        rt = runtime.Runtime(prog='dummy', working_set=working_set)
         rt.argparser  # populate the argparser
         rt.argparser_details.clear()
         with pretty_logging(
@@ -1136,7 +1148,8 @@ class PackageManagerRuntimeTestCase(unittest.TestCase):
 
         self.assertIn('CRITICAL', s.getvalue())
         self.assertIn(
-            'provided argparser not registered to this runtime.',
+            "provided argparser (prog='dummy') not associated with this "
+            "runtime (<calmjs.runtime.Runtime",
             s.getvalue())
         self.assertIn(
             'runtime cannot continue due to missing argparser details',
@@ -1646,7 +1659,7 @@ class RuntimeGoingWrongTestCase(unittest.TestCase):
         self.assertIn("CRITICAL", stderr)
         self.assertIn(
             "Runtime subclass at entry_point 'badsimple = "
-            "calmjs.testing.utils:badsimple' has overide "
+            "calmjs.testing.utils:badsimple' has override "
             "'entry_point_load_validated' without filtering out "
             "its parent classes; this can be addressed by calling "
             "super(calmjs.testing.module3.runtime.BadSimpleRuntime, self)."
@@ -1827,10 +1840,11 @@ class ArgumentHandlingTestCase(unittest.TestCase):
     def test_before_and_after_extras(self):
         rt = self.setup_runtime()
         with self.assertRaises(SystemExit):
-            rt(['-u', 'cmd', 'pkg', '-u'])
-        err = sys.stderr.getvalue().splitlines()[-1].strip()
-        # former has priority
-        self.assertEqual("calmjs: error: unrecognized arguments: -u", err)
+            rt(['--view', 'cmd', 'pkg', '-n', '-v'])
+        err = sys.stderr.getvalue()
+        # --view is recognized in calmjs cmd
+        self.assertIn("calmjs: error: unrecognized arguments: --view", err)
+        self.assertIn("calmjs cmd: error: unrecognized arguments: -n", err)
 
     def test_before_known_and_after_unknown(self):
         rt = self.setup_runtime()
@@ -1838,6 +1852,16 @@ class ArgumentHandlingTestCase(unittest.TestCase):
             rt(['-v', 'cmd', 'pkg', '-u'])
         err = sys.stderr.getvalue().splitlines()[-1].strip()
         self.assertEqual("calmjs cmd: error: unrecognized arguments: -u", err)
+
+    def test_before_known_to_after_but_not_after(self):
+        rt = self.setup_runtime()
+        with self.assertRaises(SystemExit):
+            rt(['--view', 'cmd', 'pkg'])
+        err = sys.stderr.getvalue()
+        errline = err.splitlines()[-1].strip()
+        self.assertNotIn('calmjs cmd: error:', err)
+        self.assertEqual(
+            "calmjs: error: unrecognized arguments: --view", errline)
 
     def test_before_and_after_extras_scattered(self):
         # previously test_before_and_after_extras_known_before, as
@@ -1866,6 +1890,105 @@ class ArgumentHandlingTestCase(unittest.TestCase):
         err = sys.stderr.getvalue().splitlines()[-1].strip()
         # exact message differs between py2 and py3
         self.assertIn("calmjs cmd: error: ", err)
+
+    def test_subparser_level_2_missing_argument(self):
+        stub_stdouts(self)
+        working_set = mocks.WorkingSet({
+            'calmjs.runtime': [
+                'artifact = calmjs.runtime:artifact',
+            ],
+            'calmjs.runtime.artifact': [
+                'build = calmjs.runtime:artifact_build',
+            ],
+        })
+        rt = runtime.Runtime(working_set=working_set, prog='calmjs')
+        # An underspecified command should also return False.
+        with self.assertRaises(SystemExit):
+            rt(['artifact', 'build'])
+        # ensure the help for the command itself is printed
+        self.assertIn('calmjs artifact build: error', sys.stderr.getvalue())
+
+    def test_subparser_level_2_unrecognized_argument_final(self):
+        stub_stdouts(self)
+        working_set = mocks.WorkingSet({
+            'calmjs.runtime': [
+                'artifact = calmjs.runtime:artifact',
+            ],
+            'calmjs.runtime.artifact': [
+                'build = calmjs.runtime:artifact_build',
+            ],
+        })
+        rt = runtime.Runtime(working_set=working_set, prog='calmjs')
+        # An underspecified command should also return False.
+        with self.assertRaises(SystemExit):
+            rt(['artifact', 'build', 'package', '--no-such-argument'])
+        # ensure the help for the command itself is printed
+        self.assertIn('calmjs artifact build: error', sys.stderr.getvalue())
+
+    def test_subparsers_unrecognized_argument_interspersed(self):
+        stub_stdouts(self)
+        working_set = mocks.WorkingSet({
+            'calmjs.runtime': [
+                'artifact = calmjs.runtime:artifact',
+            ],
+            'calmjs.runtime.artifact': [
+                'build = calmjs.runtime:artifact_build',
+            ],
+        })
+        rt = runtime.Runtime(working_set=working_set, prog='calmjs')
+        # An underspecified command should also return False.
+        with self.assertRaises(SystemExit):
+            rt(['--1', 'artifact', '--2', 'build', 'package', '--3'])
+        # ensure the help for the command itself is printed
+        err = sys.stderr.getvalue()
+        self.assertIn(
+            'calmjs: error: unrecognized arguments: --1', err)
+        self.assertIn(
+            'calmjs artifact: error: unrecognized arguments: --2', err)
+        self.assertIn(
+            'calmjs artifact build: error: unrecognized arguments: --3', err)
+
+    def test_subparsers_unrecognized_argument_skipped(self):
+        stub_stdouts(self)
+        working_set = mocks.WorkingSet({
+            'calmjs.runtime': [
+                'artifact = calmjs.runtime:artifact',
+            ],
+            'calmjs.runtime.artifact': [
+                'build = calmjs.runtime:artifact_build',
+            ],
+        })
+        rt = runtime.Runtime(working_set=working_set, prog='calmjs')
+        # An underspecified command should also return False.
+        with self.assertRaises(SystemExit):
+            rt(['--1', 'artifact', 'build', 'package', '--3'])
+        # ensure the help for the command itself is printed
+        err = sys.stderr.getvalue()
+        self.assertIn(
+            'calmjs: error: unrecognized arguments: --1', err)
+        self.assertNotIn('calmjs artifact: error', err)
+        self.assertIn(
+            'calmjs artifact build: error: unrecognized arguments: --3', err)
+
+    def test_subparsers_first_fail(self):
+        stub_stdouts(self)
+        working_set = mocks.WorkingSet({
+            'calmjs.runtime': [
+                'artifact = calmjs.runtime:artifact',
+            ],
+            'calmjs.runtime.artifact': [
+                'build = calmjs.runtime:artifact_build',
+            ],
+        })
+        rt = runtime.Runtime(working_set=working_set, prog='calmjs')
+        # An underspecified command should also return False.
+        with self.assertRaises(SystemExit):
+            rt(['--1', 'artifact', 'build', 'package'])
+        # ensure the help for the command itself is printed
+        err = sys.stderr.getvalue()
+        self.assertIn('calmjs: error: unrecognized arguments: --1', err)
+        self.assertNotIn('calmjs artifact: error', err)
+        self.assertNotIn('calmjs artifact build: error', err)
 
 
 class PackageManagerRuntimeAlternativeIntegrationTestCase(unittest.TestCase):

src/calmjs/argparse.py
@@ -258,3 +258,16 @@ class ArgumentParser(argparse.ArgumentParser):
                 deprecation=deprecation,
             )
         return action
+
+    def soft_error(self, message):
+        """
+        Same as error, without the dying in a fire part.
+        """
+
+        self.print_usage(sys.stderr)
+        args = {'prog': self.prog, 'message': message}
+        self._print_message(
+            _('%(prog)s: error: %(message)s\n') % args, sys.stderr)
+
+    def unrecognized_arguments_error(self, args):
+        self.soft_error(_('unrecognized arguments: %s') % ' '.join(args))

commit 3d7c1a704f5478b29f9bd2cefb9940225da43ea6

Correction to a misplaced method
- Given that this method is now used in the base class, it really should
  be implemented there.
- Correcting the defect introduced in 68e49cf1

src/calmjs/runtime.py
@@ -267,6 +267,61 @@ class BaseRuntime(BootstrapRuntime):
 
         self.argparser.error(message)
 
+    def unrecognized_arguments_error(self, args, parsed, extras):
+        """
+        This exists because argparser is dumb and naive and doesn't
+        fail unrecognized arguments early.
+        """
+
+        # loop variants
+        kwargs = vars(parsed)
+        failed = list(extras)
+        # initial values
+        runtime, subparser, idx = (self, self.argparser, 0)
+        # recursion not actually needed when it can be flattened.
+        while isinstance(runtime, Runtime):
+            cmd = kwargs.pop(runtime.action_key)
+            # can happen if it wasn't set, or is set but from a default
+            # value (thus not provided by args)
+            action_idx = None if cmd not in args else args.index(cmd)
+            if cmd not in args and cmd is not None:
+                # this normally shouldn't happen, and the test case
+                # showed that the parsing will not flip down to the
+                # forced default subparser - this can remain a debug
+                # message until otherwise.
+                logger.debug(
+                    "command for prog=%r is set to %r without being specified "
+                    "as part of the input arguments - the following error "
+                    "message may contain misleading references",
+                    subparser.prog, cmd
+                )
+            subargs = args[idx:action_idx]
+            subparsed, subextras = subparser.parse_known_args(subargs)
+            if subextras:
+                subparser.unrecognized_arguments_error(subextras)
+                # since the failed arguments are in order
+                failed = failed[len(subextras):]
+                if not failed:
+                    # have taken everything, quit now.
+                    # also note that if cmd was really None it would
+                    # cause KeyError below, but fortunately it also
+                    # forced action_idx to be None which took all
+                    # remaining tokens from failed, so definitely get
+                    # out of here.
+                    break
+
+            # advance the values
+            # note that any internal consistency will almost certainly
+            # result in KeyError being raised.
+            details = runtime.get_argparser_details(subparser)
+            runtime = details.runtimes[cmd]
+            subparser = details.subparsers[cmd]
+            idx = action_idx + 1
+
+        if failed:
+            subparser.unrecognized_arguments_error(failed)
+        sys.exit(2)
+
     def __call__(self, args=None):
         args = norm_args(args)
         # MUST use the bootstrap runtime class to process all the common
@@ -612,61 +667,6 @@ class Runtime(BaseRuntime):
             'provided argparser (prog=%r) not associated with this '
             'runtime (%r)', argparser.prog, self
         )
-
-    def unrecognized_arguments_error(self, args, parsed, extras):
-        """
-        This exists because argparser is dumb and naive and doesn't
-        fail unrecognized arguments early.
-        """
-
-        # loop variants
-        kwargs = vars(parsed)
-        failed = list(extras)
-        # initial values
-        runtime, subparser, idx = (self, self.argparser, 0)
-        # recursion not actually needed when it can be flattened.
-        while isinstance(runtime, Runtime):
-            cmd = kwargs.pop(runtime.action_key)
-            # can happen if it wasn't set, or is set but from a default
-            # value (thus not provided by args)
-            action_idx = None if cmd not in args else args.index(cmd)
-            if cmd not in args and cmd is not None:
-                # this normally shouldn't happen, and the test case
-                # showed that the parsing will not flip down to the
-                # forced default subparser - this can remain a debug
-                # message until otherwise.
-                logger.debug(
-                    "command for prog=%r is set to %r without being specified "
-                    "as part of the input arguments - the following error "
-                    "message may contain misleading references",
-                    subparser.prog, cmd
-                )
-            subargs = args[idx:action_idx]
-            subparsed, subextras = subparser.parse_known_args(subargs)
-            if subextras:
-                subparser.unrecognized_arguments_error(subextras)
-                # since the failed arguments are in order
-                failed = failed[len(subextras):]
-                if not failed:
-                    # have taken everything, quit now.
-                    # also note that if cmd was really None it would
-                    # cause KeyError below, but fortunately it also
-                    # forced action_idx to be None which took all
-                    # remaining tokens from failed, so definitely get
-                    # out of here.
-                    break
-
-            # advance the values
-            # note that any internal consistency will almost certainly
-            # result in KeyError being raised.
-            details = runtime.get_argparser_details(subparser)
-            runtime = details.runtimes[cmd]
-            subparser = details.subparsers[cmd]
-            idx = action_idx + 1
-
-        if failed:
-            subparser.unrecognized_arguments_error(failed)
-        sys.exit(2)
 
     def error(self, argparser, target, message):
         """

src/calmjs/tests/test_runtime.py
@@ -108,6 +108,13 @@ class BaseRuntimeTestCase(unittest.TestCase):
         self.assertEqual(runtime.norm_args([]), [])
         self.assertEqual(runtime.norm_args(['arg']), ['arg'])
 
+    def test_base_runtime_unknown_args(self):
+        stub_stdouts(self)
+        bt = runtime.BaseRuntime()
+        with self.assertRaises(SystemExit):
+            bt(['unknown'])
+        self.assertIn('unrecognized arguments: unknown', sys.stderr.getvalue())
+
     def test_global_flags(self):
         def fake_parse(args):
             warnings.warn('fake deprecation', DeprecationWarning)